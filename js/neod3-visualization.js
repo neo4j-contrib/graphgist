32b94412303fba499c04c08778c8f9252dff23af
Anders Nawroth <anders@neotechnology.com>
2014-06-01 23:56:12 +0200

    Work on the integration of visualization, added download action for neod3 graphs.

--- js/neod3-visualization.js
+++ js/neod3-visualization.js
@@ -1,125 +1,186 @@
-var styleContents =
-    "node {\
-      diameter: 40px;\
-      color: #DFE1E3;\
-      border-color: #D4D6D7;\
-      border-width: 2px;\
-      text-color-internal: #000000;\
-      caption: '{name}';\
-      font-size: 10px;\
-    }\
-    relationship {\
-      color: #4356C0;\
-      shaft-width: 3px;\
-      font-size: 9px;\
-      padding: 3px;\
-      text-color-external: #000000;\
-      text-color-internal: #FFFFFF;\
-    }\n";
+function Neod3Renderer() {
 
-var skip = ["id", "start", "end", "source", "target", "labels", "type", "selected"];
-var prio_props = ["name", "title", "tag"];
+    var styleContents =
+        "node {\
+          diameter: 40px;\
+          color: #DFE1E3;\
+          border-color: #D4D6D7;\
+          border-width: 2px;\
+          text-color-internal: #000000;\
+          caption: '{name}';\
+          font-size: 10px;\
+        }\
+        relationship {\
+          color: #4356C0;\
+          shaft-width: 3px;\
+          font-size: 9px;\
+          padding: 3px;\
+          text-color-external: #000000;\
+          text-color-internal: #FFFFFF;\
+        }\n";
 
-function renderNeod3(id, $container, visualization) {
-    function extract_props(pc) {
-        var p = {};
-        for (var key in pc) {
-            if (!pc.hasOwnProperty(key) || skip.indexOf(key) != -1) continue;
-            p[key] = pc[key];
-        }
-        return p;
-    }
+    var skip = ["id", "start", "end", "source", "target", "labels", "type", "selected"];
+    var prio_props = ["name", "title", "tag"];
 
-    function node_styles(nodes) {
-        function label(n) {
-            var labels = n["labels"];
-            if (labels && labels.length) {
-                return labels[labels.length - 1];
+    var serializer = null;
+
+    var $downloadSvgLink = $('<a href="#" class="btn btn-success visualization-download" target="_blank"><i class="icon-download-alt"></i> Download SVG</a>').hide().click(function () {
+        $downloadSvgLink.hide();
+    });
+    var downloadSvgLink = $downloadSvgLink[0];
+
+    function render(id, $container, visualization) {
+        function extract_props(pc) {
+            var p = {};
+            for (var key in pc) {
+                if (!pc.hasOwnProperty(key) || skip.indexOf(key) != -1) continue;
+                p[key] = pc[key];
             }
-            return "";
+            return p;
         }
 
-        var style = {};
-        for (var i = 0; i < nodes.length; i++) {
-            nodes[i].properties = extract_props(nodes[i]);
-            var keys = Object.keys(nodes[i].properties);
-            if (label(nodes[i]) != "" && keys.length > 0) {
-                keys = prio_props.filter(function (k) {
-                    return keys.indexOf(k) != -1
-                }).concat(keys);
-                var selector = "node." + label(nodes[i]);
-                style[selector] = style[selector] || keys[0];
+        function node_styles(nodes) {
+            function label(n) {
+                var labels = n["labels"];
+                if (labels && labels.length) {
+                    return labels[labels.length - 1];
+                }
+                return "";
+            }
+
+            var style = {};
+            for (var i = 0; i < nodes.length; i++) {
+                nodes[i].properties = extract_props(nodes[i]);
+                var keys = Object.keys(nodes[i].properties);
+                if (label(nodes[i]) != "" && keys.length > 0) {
+                    keys = prio_props.filter(function (k) {
+                        return keys.indexOf(k) != -1
+                    }).concat(keys);
+                    var selector = "node." + label(nodes[i]);
+                    style[selector] = style[selector] || keys[0];
+                }
+            }
+            return style;
+        }
+
+        function style_sheet(style, styleContents) {
+            var styleSheet = "";
+            var c = 1;
+            var colors = neo.style.defaults.colors;
+            for (var k in style) {
+                var color = colors[c];
+                c = (c + 1) % colors.length
+                styleSheet += k + " {caption: '{" + style[k] + "}'; color: " + color.color +
+                    "; border-color: " + color['border-color'] +
+                    "; text-color-internal: " + color['text-color-internal'] +
+                    "; text-color-external: " + color['text-color-internal'] +
+                    "; }\n";
+            }
+            return styleContents + styleSheet;
+        }
+
+        function applyZoom() {
+            renderer.select(".nodes").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+            renderer.select(".relationships").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        }
+
+        function zoomStart() {
+            if (d3.event.sourceEvent.altKey) {
+                zoomBehavior.on("zoom", applyZoom);
+                renderer.on("mousedown.zoom", null)
+            }
+            else {
+                zoomBehavior
+                    .on("mousedown.zoom", null)
+                    .on("mousewheel.zoom", null)
+                    .on("mousemove.zoom", null)
+                    .on("DOMMouseScroll.zoom", null)
+                    .on("dblclick.zoom", null)
+                    .on("touchstart.zoom", null)
+                    .on("touchmove.zoom", null)
+                    .on("touchend.zoom", null);
             }
         }
-        return style;
-    }
 
-    function style_sheet(style, styleContents) {
-        var styleSheet = "";
-        var c = 1;
-        var colors = neo.style.defaults.colors;
-        for (var k in style) {
-            var color = colors[c];
-            c = (c + 1) % colors.length
-            styleSheet += k + " {caption: '{" + style[k] + "}'; color: " + color.color +
-                "; border-color: " + color['border-color'] +
-                "; text-color-internal: " + color['text-color-internal'] +
-                "; text-color-external: " + color['text-color-internal'] +
-                "; }\n";
+        var links = visualization.links;
+        var nodes = visualization.nodes;
+        for (var i = 0; i < links.length; i++) {
+            links[i].source = links[i].start;
+            links[i].target = links[i].end;
+            links[i].properties = props(links[i]);
         }
-        return styleContents + styleSheet;
-    }
+        var nodeStyle = node_styles(nodes);
+        var styleSheet = style_sheet(nodeStyle, styleContents)
 
-    function applyZoom() {
-        renderer.select(".nodes").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-        renderer.select(".relationships").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-    }
+        var graphModel = neo.graphModel()
+            .nodes(nodes)
+            .relationships(links);
+        var dummyFunc = function () {
+        };
+        var graphView = neo.graphView()
+            .style(styleSheet)
+            .width($container.width()).height($container.height()).on('nodeClicked', dummyFunc).on('relationshipClicked', dummyFunc).on('nodeDblClicked', dummyFunc);
+        var renderer = d3.select("#" + id).append("svg").data([graphModel]);
+        var zoomBehavior = d3.behavior.zoom().on("zoomstart", zoomStart).scaleExtent([0.2, 8])
 
-    function zoomStart() {
-        if (d3.event.sourceEvent.altKey) {
-            zoomBehavior.on("zoom",applyZoom);
-            renderer.on("mousedown.zoom", null)
-        }
-        else {
-            zoomBehavior
-            .on("mousedown.zoom", null)
-            .on("mousewheel.zoom", null)
-            .on("mousemove.zoom", null)
-            .on("DOMMouseScroll.zoom", null)
-            .on("dblclick.zoom", null)
-            .on("touchstart.zoom", null)
-            .on("touchmove.zoom", null)
-            .on("touchend.zoom", null);
-        }
-    }
-
-    var links = visualization.links;
-    var nodes = visualization.nodes;
-    for (var i = 0; i < links.length; i++) {
-        links[i].source = links[i].start;
-        links[i].target = links[i].end;
-        links[i].properties = props(links[i]);
-    }
-    var nodeStyle = node_styles(nodes);
-    var styleSheet = style_sheet(nodeStyle, styleContents)
-
-    var graphModel = neo.graphModel()
-        .nodes(nodes)
-        .relationships(links);
-    var dummyFunc = function () {
-    };
-    var graphView = neo.graphView()
-        .style(styleSheet)
-        .width($container.width()).height($container.height()).on('nodeClicked', dummyFunc).on('relationshipClicked', dummyFunc).on('nodeDblClicked', dummyFunc);
-    var renderer = d3.select("#" + id).append("svg").data([graphModel]);
-    var zoomBehavior = d3.behavior.zoom().on("zoomstart", zoomStart).scaleExtent([0.2,8])
-
-    renderer.call(graphView);
-    renderer.call(zoomBehavior);
-
-    return function () {
-        graphView.height($container.height());
-        graphView.width($container.width());
         renderer.call(graphView);
-    };
+        renderer.call(zoomBehavior);
+
+        function refresh() {
+            graphView.height($container.height());
+            graphView.width($container.width());
+            renderer.call(graphView);
+        }
+
+        function saveToSvg() {
+            var svgElement = $('#' + id).children('svg').first()[0];
+            var xml = serializeSvg(svgElement, $container);
+            if (downloadSvgLink.href !== '#') {
+                window.URL.revokeObjectURL(downloadSvgLink.href);
+            }
+            var blob = new window.Blob([xml], {
+                'type': 'image/svg+xml'
+            });
+            downloadSvgLink.href = window.URL.createObjectURL(blob);
+            $downloadSvgLink.appendTo($container).show();
+            $downloadSvgLink.attr('download', id + '.svg');
+        }
+
+        function getFunctions() {
+            var funcs = {};
+            if (window.Blob) {
+                funcs['icon-download-alt'] = saveToSvg
+            }
+            return funcs;
+        }
+
+        return  {
+            'subscriptions': {
+                'expand': refresh,
+                'contract': refresh,
+                'sizeChange': refresh
+            },
+            'actions': getFunctions()
+        };
+    }
+
+    function serializeSvg(element, $container) {
+        if (serializer === null) {
+            if (typeof window.XMLSerializer !== 'undefined') {
+                var xmlSerializer = new XMLSerializer();
+                serializer = function (emnt) {
+                    return xmlSerializer.serializeToString(emnt);
+                };
+            } else {
+                serializer = function (emnt) {
+                    return '<svg xmlns="http://www.w3.org/2000/svg">' + $(emnt).html() + '</svg>';
+                }
+            }
+        }
+        var svg = serializer(element);
+        svg = svg.replace('<svg ', '<svg height="' + $container.height() + '" width="' + $container.width() + '" style="font-family: sans-serif;" ');
+        return svg;
+    }
+
+    return {'render': render};
 }
