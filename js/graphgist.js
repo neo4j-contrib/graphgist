412caa94dd987a2378d5242a4361ce1626fdc666
Michael Hunger <github@jexp.de>
2014-06-06 21:46:23 +0200

    more 2.1 versions

--- js/graphgist.js
+++ js/graphgist.js
@@ -37,7 +37,7 @@ function GraphGist($) {
     var $VISUALIZATION_ICONS = $('<div class="visualization-icons"><i class="icon-fullscreen fullscreen-icon" title="Toggle fullscreen mode"></i></div>');
     var $I = $('<i/>');
 
-    var DEFAULT_VERSION = '2.0.0';
+    var DEFAULT_VERSION = '2.1';
     var CONSOLE_VERSIONS = { '2.0.0-M06': 'http://neo4j-console-20m06.herokuapp.com/',
         '2.0.0-RC1': 'http://neo4j-console-20rc1.herokuapp.com/',
         '2.0.0': 'http://neo4j-console-20.herokuapp.com/',
@@ -45,6 +45,8 @@ function GraphGist($) {
         '2.0.2': 'http://neo4j-console-20.herokuapp.com/',
         '2.0.3': 'http://neo4j-console-20.herokuapp.com/',
         '2.1.0': 'http://neo4j-console-21.herokuapp.com/',
+        '2.1.1': 'http://neo4j-console-21.herokuapp.com/',
+        '2.1': 'http://neo4j-console-21.herokuapp.com/',
         'local': 'http://localhost:8080/',
         '1.9': 'http://neo4j-console-19.herokuapp.com/'
     }
